a96cefd7794a25aef09bdbefb57875ab4f8ebeef
Hans912 <hanshelmrich912@gmail.com>
2024-11-18 15:18:40 -0500

    new

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://gymlog-front.onrender.com', // Replace with your frontend URL
+  }));
 app.use(express.json());
 
 // Routes
